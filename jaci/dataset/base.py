02835bc4ebc00e74b0b9abc093730cd5cf66dbbb
Gustavo  van Erven <gvanerven@blackpearl.local>
2019-11-26 17:08:03 -0300

    dir structures

---
+++ jaci/dataset/base.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+class Dataset:
+    self.dataset = None
+
+    def __init__(self):
+        pass
+
+    def features(self):
+        pass
+
+    def shape(self):
+        pass
+
+    def next(self):
+        pass
+
+    def fetch_all(self):
+        pass
+
+    def read_batch(self, size_batch):
+        pass
