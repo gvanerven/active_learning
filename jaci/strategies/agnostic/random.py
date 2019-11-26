9ac0b0ef5417026c8ed587bd472b81529d54d798
Gustavo  van Erven <gvanerven@blackpearl.local>
2019-11-26 19:51:54 -0300

    included the first strategy

--- jaci/strategies/agnostic/random.py
+++ jaci/strategies/agnostic/random.py
@@ -1,2 +1,10 @@
 # -*- coding: utf-8 -*-
+from jaci.strategies import Strategy
 
+class RandomSampling(Strategy):
+
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    def query_next(self):
+        pass
